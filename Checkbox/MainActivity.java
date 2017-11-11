559af43f06849ad3c7aac895b9f61c6511d0337c
noodleredhead <32986786+noodleredhead@users.noreply.github.com>
2017-11-10 20:20:58 -0600

    Checkbox

---
+++ Checkbox/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.alondra.aplicacion;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+    //variables//
+    Button btn;
+    CheckBox checkBox1,checkBox2,checkBox3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+        checkBox1 = (CheckBox) findViewById(R.id.checkBox1);
+        checkBox2 = (CheckBox) findViewById(R.id.checkBox2);
+        checkBox3 = (CheckBox) findViewById(R.id.checkBox3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClickMe(view);
+            }
+        });
+    }
+
+    public void ClickMe(View v)
+    {
+        if (checkBox1.isChecked() == true &&checkBox2.isChecked() == true && checkBox3.isChecked() == true)
+        {
+            getWindow().getDecorView().setBackgroundColor(Color.parseColor("#FFFFFF"));
+        }
+        else if (checkBox1.isChecked() == true && checkBox2.isChecked() == true)
+        {
+            getWindow().getDecorView().setBackgroundColor(Color.parseColor("#FFFF00"));
+        }
+        else if (checkBox1.isChecked() == true && checkBox3.isChecked() == true)
+        {
+            getWindow().getDecorView().setBackgroundColor(Color.parseColor("#FF00FF"));
+        }
+        else if (checkBox2.isChecked() == true && checkBox3.isChecked() == true)
+        {
+            getWindow().getDecorView().setBackgroundColor(Color.parseColor("#00FFFF"));
+        }
+        else if (checkBox1.isChecked() == true)
+        {
+            getWindow().getDecorView().setBackgroundColor(Color.parseColor("#FF0000"));
+        }
+        else if (checkBox2.isChecked() == true)
+        {
+            getWindow().getDecorView().setBackgroundColor(Color.parseColor("#00FF00"));
+        }
+        else if (checkBox3.isChecked() == true)
+        {
+            getWindow().getDecorView().setBackgroundColor(Color.parseColor("#0000FF"));
+        }
+        else
+        {
+            getWindow().getDecorView().setBackgroundColor(Color.parseColor("#000000"));
+        }
+    } //Final del "OnCreate"//
+
+
+} //Fin de Main Activity//
